9d11d181195319dd4def19a1a6af5bba62eecf48
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-19 20:19:34 +0530

    weakMap

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-for(var i=0; i<3;i++){
-    setTimeout(()=>console.log(i),200);
-}
-for (var i = 0; i < 3; i++) {
-  setTimeout(_ => console.log(`We’re at ${i}`), 100);
-}
-var a = 20;
-function change(){
-    var a = 10;
-    console.log(a);
-}
-change();
-console.log(a);
+// for(var i=0; i<3;i++){
+//     setTimeout(()=>console.log(i),200);
+// }
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(_ => console.log(`We’re at ${i}`), 100);
+// }
+// var a = 20;
+// function change(){
+//     var a = 10;
+//     console.log(a);
+// }
+// change();
+// console.log(a);
+// {
+// let obj1 = {
+//     name :"obj1"
+// }
+// let obj2 = {
+//     name :"obj2"
+// }
+// let obj3 = {
+//     name :"obj3"
+// }
+// var myWeakMap = new WeakMap();
+// myWeakMap.set(obj1,1).set(obj2,2).set(obj3,3);
+// var myWeakSet = new WeakSet();
+// myWeakSet.add(obj1).add(obj2).add(obj3);
+// var myMap = new Map();
+// myMap.set('abcd',1).set('efgh',2).set('jklm',3);
+// }
+
+// console.log(myWeakMap);
+// console.log(myWeakSet);
+// console.log(myMap);
+
+
+
+
 {
-let obj1 = {
-    name :"obj1"
+    let x = {
+        a:[1,2]
+    };
+    var myweakMap = new WeakMap();
+    myweakMap.set(x,'object1');
+    
 }
-let obj2 = {
-    name :"obj2"
-}
-let obj3 = {
-    name :"obj3"
-}
-var myWeakMap = new WeakMap();
-myWeakMap.set(obj1,1).set(obj2,2).set(obj3,3);
-var myWeakSet = new WeakSet();
-myWeakSet.add(obj1).add(obj2).add(obj3);
-var myMap = new Map();
-myMap.set('abcd',1).set('efgh',2).set('jklm',3);
-}
-
-console.log(myWeakMap);
-console.log(myWeakSet);
-console.log(myMap);
-
-
-
+x = null;
+console.log(myweakMap);
