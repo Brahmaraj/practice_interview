d1ee4ba9c375f3b5fe59d2dfef9718649475349b
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-20 23:54:18 +0530

    callbacks

--- callbacks.js
+++ callbacks.js
@@ -1,7 +1,13 @@
 let add = (x,y) => x+y;
 
+let sub = (x,y) => x-y;
+
+let mul = (x,y) => x*y;
+
 function operation(logic,operand1,operand2){
     return logic(operand1,operand2);
 }
 
 console.log(operation(add,3,5));
+console.log(operation(sub,3,5));
+console.log(operation(mul,3,5));
