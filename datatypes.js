2c4bd441c2b1f7bb50e681090a8e9cd8fe2b5957
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-21 00:18:00 +0530

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -4,11 +4,13 @@ let num = 23;//Number
 let bool = true;//Boolean
 let undef = undefined;//Undefined
 let big = 1n;//Big Int
-let num1 = Number.MAX_SAFE_INTEGER;
 
-console.log(num1);
+let nil = null;
 
-const big_num = 9007199254740995;
-const big_int = 9007199254740995n;
-console.log(big_num);
-console.log(big_int);
+
+// let num1 = Number.MAX_SAFE_INTEGER;
+// console.log(num1);
+// const big_num = 9007199254740995;
+// const big_int = 9007199254740995n;
+// console.log(big_num);
+// console.log(big_int);
