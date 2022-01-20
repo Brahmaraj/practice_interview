d3ae9ef35ffd7107a1057c3abb739f95fa4dad7d
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-21 00:33:15 +0530

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -6,10 +6,20 @@ let undef = undefined;//Undefined
 let big = 1n;//Big Int
 let nil = null;
 let sym = Symbol("try");
+let obj = {x:"first property"};
 
-console.log(sym);
-let cmp_sym = Symbol("try");
-console.log(sym==cmp_sym);//false since Symbol always returns unique value
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof bool);
+console.log(typeof big);
+console.log(typeof undef);
+console.log(typeof nil);//will print object and not null due to legacy reasons
+console.log(typeof sym);
+console.log(typeof obj);
+
+// console.log(sym);
+// let cmp_sym = Symbol("try");
+// console.log(sym==cmp_sym);//false since Symbol always returns unique value
 // let num1 = Number.MAX_SAFE_INTEGER;
 // console.log(num1);
 // const big_num = 9007199254740995;//not accurate
