6802e4cd98a643ac9e3ba7f5c942734d2f934962
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-20 12:45:33 +0530

    recursion and prototypes

--- prototype.js
+++ prototype.js
@@ -25,4 +25,28 @@ function factorial(n){
     return 1;
 }
 
+function mirror_no(n){
+    console.log(n);
+    if(n>1)
+        mirror_no(n-1);
+    console.log(n);
+}
+
+mirror_no(4);
+
 console.log(factorial(3));
+
+array1 = [1,2,3,4,5,6,7,8,9,10];
+
+let square = (ele) => ele*ele ;
+
+// array1.forEach(element => {
+//     console.log(square(element));
+// });
+
+console.log(array1.map(square))
+console.log(array1.filter(ele=>ele>5))
+console.log(array1.reduce((prev,curr)=>{
+    prev += curr;
+    return prev;
+},10))
--- scopes.js
+++ scopes.js
@@ -51,3 +51,4 @@ function first(){
 }
 first()(); // due to this first is popped of the stack and second is pushed as after return 
             // there is nothing so the returned function is now added to stack !!!!
+
