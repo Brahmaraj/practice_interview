eb5aa77806ebf14ba5aab9820195e4abed43b9ce
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-21 00:13:28 +0530

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -3,6 +3,12 @@ let str = "string";//String
 let num = 23;//Number
 let bool = true;//Boolean
 let undef = undefined;//Undefined
-let big = 6n;//Big Int
+let big = 1n;//Big Int
 let num1 = Number.MAX_SAFE_INTEGER;
+
 console.log(num1);
+
+const big_num = 9007199254740995;
+const big_int = 9007199254740995n;
+console.log(big_num);
+console.log(big_int);
