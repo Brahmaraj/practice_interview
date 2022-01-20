389a5b38ff7a3f477be2f9c0e1cb15e6cfeec70a
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-20 12:24:00 +0530

    prototypes and __proto__ (dunder proto)

--- prototype.js
+++ prototype.js
@@ -8,8 +8,21 @@ function House(color,size,expensive){
     this.color = color;
     this.expensive = expensive;
 }
-
+function prototypeCheck(){
+    
+}
 var myhouse = new House("red","very big",true);
 console.log(Object.getPrototypeOf(myhouse));//__proto__ -> poitning to House's prototype
-console.log(House.prototype)
-console.log(house.prototype);
+console.log(myhouse.__proto__)
+console.log(prototypeCheck.__proto__.__proto__)
+console.log(House.__proto__);//in javascript everything is object!!
+console.log(Object.prototype)
+//console.log(house.prototype);
+
+function factorial(n){
+    while(n>1)
+    return n*factorial(n-1)
+    return 1;
+}
+
+console.log(factorial(3));
