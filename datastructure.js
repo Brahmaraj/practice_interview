39ea4804c37da00356b17baf78fe55984de7ca27
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-22 01:01:37 +0530

    weak maps and weak set, destructuring and rest parameters

--- datastructure.js
+++ datastructure.js
@@ -8,10 +8,19 @@ let obj1 = {
 my_weak_set.add(obj1);
 }
 
-let obj2 = {
-    scope:"global"
+let person1 = {
+    name :"brahma",
+    l_name:"rathod",
+    age:21,
+    sex:"M",
+    interests:"web development, cyber security, block chain etc"
 }
-my_weak_map.set(obj2,"1");
-obj2=null;
-console.log(my_weak_map);
-console.log(my_weak_set);
+// my_weak_map.set(person1,"1");
+// obj2=null;
+// console.log(my_weak_map);
+// console.log(my_weak_set);
+//Object Destructuring
+const {name, l_name, ...lastthree} = person1; // has to be same as the property name
+console.log(name);
+console.log(l_name)
+console.log(lastthree)
