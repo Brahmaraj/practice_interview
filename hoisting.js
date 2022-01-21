abd95f2a5f68f3382b0805ab82ad34cb18f4cc32
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-21 23:55:52 +0530

    hoisting and type coercion and conversion

--- hoisting.js
+++ hoisting.js
@@ -17,9 +17,9 @@ console.log(dec_only+Number(str_num));//string converted to number and adder Exp
 // var dec_reinit = 80;//works because var is designed that way
 // console.log(dec_reinit);
 
-// dec_update = 70;//updating is possible in LET
-// console.log(dec_update)
-//let dec_update = 70; syntax error identifier already declared
+dec_update = 70;//updating is possible in LET
+console.log(dec_update)
+let dec_update = 70;// syntax error identifier already declared
 
 const dec_only = 60;//syntax error identifier already declared
 dec_only = 60;//type-error assignment to constant variable
