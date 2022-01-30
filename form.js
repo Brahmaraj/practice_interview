7dc7f420f2ac5b28fa89765906f658e959425511
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-30 12:07:33 +0530

    form

---
+++ form.js
@@ -0,0 +1,6 @@
+function display(){
+    console.log(document.getElementById("text_inp1").value);
+    console.log(document.getElementById("text_inp2").value);
+    console.log(document.getElementById("text_inp3").value);
+    console.log(document.getElementById("text_inp4").value);
+}
